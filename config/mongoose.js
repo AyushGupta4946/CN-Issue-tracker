9435f6046b74c51255810211d6693db6c6ad55af
Ayush Gupta <aagm0404@gmail.com>
2023-07-07 23:27:41 +0530

    update databse

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,11 @@
 //require the library
 const mongoose=require('mongoose');
 
-// mongoose.connect("mongodb+srv://aagm0404:1Yu4G13sc3rA19Zu@issue-tracker-cluster-d.aozwedf.mongodb.net/?retryWrites=true&w=majority"
-// );
-mongoose.connect("mongodb://127.0.0.1:27017/demo",{
-    useUnifiedTopology:true,
-    useNewUrlParser : true
-});
+mongoose.connect("mongodb+srv://aagm0404:Simraj0404@cluster0.ihza8od.mongodb.net/?retryWrites=true&w=majority")
+// mongoose.connect("mongodb://127.0.0.1:27017/demo",{
+//     useUnifiedTopology:true,
+//     useNewUrlParser : true
+// });
 const db=mongoose.connection;
 
 //error
